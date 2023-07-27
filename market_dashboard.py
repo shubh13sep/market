dfda650fdb39a6d58a7311c174731228144927fc
shubh13sep <shubhamsethi@outlook.com>
2023-07-27 16:08:29 +0530

    Update market_dashboard.py

--- market_dashboard.py
+++ market_dashboard.py
@@ -2,3 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
+
+st.sidebar.subheader('Chart Options')
+ticker = st.sidebar.text_input('Ticker', value = "AAPL")
+theme = st.sidebar.selectbox('Theme', options = ['dark', 'light'])
+fund_type = st.sidebar.selectbox('Fundamental Display', options = ['regular', 'compact'])
